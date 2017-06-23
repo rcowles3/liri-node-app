7940b454bf6108f8493f7a89b1fe196c48dbac63
rcowles3 <ryan.cowles3@gmail.com>
2017-06-23 16:17:26 -0700

    working spotify functionality

--- javascript/keys.js
+++ javascript/keys.js
@@ -1,12 +1,8 @@
+// This file will contain all nessesary acces keys required for application to run
+
 // console.log('keys are loaded');
 
-// exports.twitterKeys = {
-//   consumer_key: 'oZgEcszPWhUJnMAYhUb0LAPcX',
-//   consumer_secret: 'ODeUMNouJ00gYVCbyxH3Xtcha4sUtWYn2vIJyajYygodFt2ocB',
-//   access_token_key: '506978475-fZDwxJfN0VI3DRiSJlE8Y2yLxzh3ovgVdBkPjslS',
-//   access_token_secret: 'LW5ZOwqQtkoa1LcvSx2ueeObIV1P81tfOPaiwfJIiqjh8',
-// }
-
+// twitter access keys
 var Twitter = require('twitter');
 
 var client = new Twitter({
@@ -16,4 +12,18 @@ var client = new Twitter({
   access_token_secret: 'LW5ZOwqQtkoa1LcvSx2ueeObIV1P81tfOPaiwfJIiqjh8'
 });
 
-module.exports = client;
+// module.exports = client;
+
+
+
+// spotify access keys
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: 'a2be8dbef5ec4b30aca4ae519cc32c14',
+  secret: '36bca0118f484673bf6fbb53cf21cdbb'
+});
+
+// module.exports = spotify;
+
+module.exports = {client, spotify};
--- javascript/liri.js
+++ javascript/liri.js
@@ -11,7 +11,7 @@
 // ====================================================
 
 var fs = require('fs'); // file system
-var twitterKeys = require('./keys.js');
+var keys = require('./keys.js');
 var twitter = require('twitter');
 var spotify = require('node-spotify-api');
 var request = require('request');
@@ -19,10 +19,11 @@ var request = require('request');
 // FUNCTIONS
 // ====================================================
 
-// console.log(twitterKeys); // log to test keys successfully loaded
-
 // function to get latest tweets from account, 20
 var getTweets = function() {
+
+    // twitter keys
+    let twitterKeys = keys.client;
 
     // var to hold twitter user name parameters
     let params = { screen_name: 'ignortainment', count: 20 };
@@ -58,6 +59,43 @@ var getTweets = function() {
     });
 };
 
+// function to search spotify for song info
+var searchSpotify = function() {
+
+    // Return:
+    /*
+		Artist(s)
+		The song's name
+		A preview link of the song from Spotify
+		The album that the song is from
+	*/
+
+    // spotify keys
+    let spotify = keys.spotify;   
+
+    spotify.search({ type: 'track', limit: 1, query: 'time bomb' }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+    // variables for track info, q is short for the query of whats searched
+    let qArtist = data.tracks.items[0].artists[0].name;
+    let qTrack = data.tracks.items[0].name;
+    let qAlbum = data.tracks.items[0].album.name;
+    let qSongLink = data.tracks.items[0].artists[0].external_urls.spotify;
+
+        // console.log(data); // raw data object
+
+        // display data to command line
+        console.log("\nHere is the information that you requested from your Spotify search.");
+        console.log("\nArtist: " + qArtist);
+        console.log("Name Of Song: " + qTrack);
+        console.log("Album: " + qAlbum);
+        console.log("Preview Url: " + qSongLink + "\n");
+       
+    });
+}
 
 // calling our functions
-getTweets();
+// getTweets();
+searchSpotify();
